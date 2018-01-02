6d21014449268d192ad646e28ffa61da507d82c3
Ya <qsnhywadej@gmail.com>
2018-01-02 16:23:03 +0500

    First commit with some bug

---
+++ README.md
@@ -0,0 +1,26 @@
+# Tic-Tac-Toe
+
+This is multiplayer Tic-Tac-Toe game 3x3 with chat.
+
+Player send link to other player. Second player join via invite link, than play started.
+
+### Tic-Tac-Toe rules:
+
+This is game for two players, who take turns marking the spaces in? for example, a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game. 
+
+[In detail](http://lmgtfy.com/?q=tic+tac+toe+rules)
+
+
+##Technologies used:
+
+* NodeJS
+* ReactJS
+* Socket.io
+* Express
+
+## Running the application
+
+* Install NodeJS, if it not installed yet.
+* npm install
+* npm start
+* Go to http://localhost:3000 to view the game.
---
+++ server.js
@@ -0,0 +1,83 @@
+//Dependency
+var express = require('express'),
+		app = express(),
+		http = require('http');
+		
+const PORT = process.env.PORT || 3000;
+const INDEX = __dirname + '/app/index.html';
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(PORT, () => { console.log("Server starts on http://localhost:"+ PORT+" !")});  
+
+io.on('connection', (socket)=> {
+	var all_rooms=[];
+	var room_name;
+	var symbol;
+	if (socket.current_room_name !== undefined) {
+		socket.leave(socket.current_room_name);
+		room_name=socket.id.toString();
+		socket.join(room_name);
+		symbol='X';
+	}
+	else
+	{
+		var urlapi = require('url'),
+		url = urlapi.parse(socket.handshake.headers.referer);
+		var itog_socketid = url.pathname.slice(1, -1);    
+		if(itog_socketid==="")
+		{
+			room_name=socket.id.toString();
+			socket.join(room_name);
+			symbol='X';
+		}    
+		else
+		{
+			room_name=itog_socketid;
+			var room = io.sockets.adapter.rooms[room_name];
+			if(room !== undefined)
+			{
+				var number_of_clients_in_room = io.sockets.adapter.rooms[room_name].length;
+				if(number_of_clients_in_room===1)
+				{
+					socket.join(room_name);
+					symbol='O';
+					socket.broadcast.to(room_name).emit('second_player');  
+				}       
+				else
+				{
+					room_name=socket.id.toString();
+					socket.join(room_name);
+					symbol='X';      
+				}         
+			}
+			else
+			{
+				room_name=socket.id.toString();
+				socket.join(room_name);
+				symbol='X';
+			}
+		}
+	}
+	socket.emit('your_player_symbol_and_current_room', {my_symbol: symbol, room: room_name} );     
+	socket.on('disconnect', (socket) => {
+		io.to(room_name).emit('player_leaves');
+		 var rooms = io.sockets.adapter.sids[socket.id];
+		 for(var room in rooms) {
+				 socket.leave(room);
+		 };
+	})
+	socket.on('new-message', (msg)=> {
+		io.to(room_name).emit('receive-message',msg); 
+	});
+});
+
+app.use('/', express.static(__dirname + '/app'));
+app.get('/', function(req, res){
+	res.sendFile(__dirname + "/app/index.html");
+});
+
+app.use('/([a-zA-Z0-9_-]{20})/', express.static(__dirname + '/app'));
+app.get('/([a-zA-Z0-9_-]{20})/', function(req, res){
+	res.sendFile(__dirname + "/app/index.html");
+});
